d0f2a1b21242beb73325ace31a69a29238222e81
Leonard Mpande <leo.august27@gmail.com>
2020-12-12 04:05:47 +0300

    user login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FirebaseDataService } from './shared/services/firebase-data.service';
+import { Store } from '@ngrx/store';
+import { ApplicationState } from './store';
+import { Router } from '@angular/router';
+import { AuthService } from './shared/services/auth-service';
+import { UpsertUser } from './store/user/actions/user.actions';
+import { Go } from './store/router/router.action';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'signboard-private-operator';
+  title = 'Sign Board Management System';
+  constructor(
+    private dataService: FirebaseDataService,
+    private store: Store<ApplicationState>,
+    public router: Router,
+    private authService: AuthService){
+      dataService.app.auth().onAuthStateChanged(user => {
+        if (user) {
+          console.log("User",user.uid);
+          // this.store.dispatch(new SetCurrentUser(user.uid));
+          this.dataService.getOne([{
+            left: 'uid', operator: '==', right: user.uid
+          }], 'users').then((data: any) => {
+            if (data.uid) {
+              // this.store.dispatch(new UpsertCurrentUser({currentUser: data}));
+              // this.store.dispatch(new UpsertUser({user : data}));
+              if (this.router.url === '/login') {
+                if (data.user_level === 'normal') {
+                  this.store.dispatch(new Go({path: ['/apply']}));
+                } else {
+                  this.store.dispatch(new Go({path: ['/dashboard']}));
+                }
+              } else {
+                this.store.dispatch(new Go({path: [this.router.url]}));
+              }
+            } else {
+              if (this.router.url === '/login') {
+                this.store.dispatch(new Go({path: ['/dashboard']}));
+              } else {
+                this.store.dispatch(new Go({path: [this.router.url]}));
+              }
+              // this.saveNewUser(user);
+            }
+          });
+        } else {
+          this.store.dispatch(new Go({path: ['login']}));
+        }
+      });
+  
+  }
+
+  
 }
--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth-service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { UserService } from '../shared/services/model-services/user.service';
+import { Store, select } from '@ngrx/store';
+import { ApplicationState } from '../store';
+import { routeAnimations } from '../shared/animations/router-animation';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../store/user/reducers/user';
+import { FirebaseDataService } from '../shared/services/firebase-data.service';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
-  styleUrls: ['./page.component.scss']
+  styleUrls: ['./page.component.scss'],
+  animations: [routeAnimations]
 })
-export class PageComponent implements OnInit {
+export class PageComponent  {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+  .observe(Breakpoints.Handset)
+  .pipe(map(result => result.matches));
 
-  constructor() { }
-
-  ngOnInit(): void {
+isOpen = true;
+user$!: Observable<User>;
+loading$!: Observable<boolean>;
+  constructor(
+    private authService: AuthService,
+    private breakpointObserver: BreakpointObserver,
+    private userService: UserService,
+    private store: Store<ApplicationState>,
+    private dataService: FirebaseDataService
+  ) {
+    // this.user$ = this.store.pipe(select(selectCurrentUser));
   }
 
+  logout() {
+    this.dataService.logout();
+  }
 }
